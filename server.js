8f369ac9d28daccc2ca3e32c4a9ce4548fba39ac
Hitesh <h08rajbhar@gmail.com>
2023-01-23 13:39:29 +0530

    adding cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
+const cors = require('cors');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 const connectDB = require('./config/db');
 const productRoute = require('./routes/productRoute');
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // routes middleware
 app.use('/api/products', productRoute);
