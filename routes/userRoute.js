7bd0f851b536adee92fdb9049a9cc8001d3d202e
Hitesh <h08rajbhar@gmail.com>
2023-01-23 22:00:08 +0530

    update user profile controller

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,12 +3,16 @@ const {
   loginUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } = require('../controllers/userController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.route('/').post(registerUser);
 router.route('/login').post(loginUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 module.exports = router;
