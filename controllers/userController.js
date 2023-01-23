db8b9d5f795287ccaca8e6620a0f2dc2608caedd
Hitesh <h08rajbhar@gmail.com>
2023-01-23 19:04:46 +0530

    register controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,37 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const generateToken = require('../utils/generateToken');
 
+// register user  => /api/users
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 // login user  => /api/users/login
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -42,4 +73,5 @@ const getUserProfile = asyncHandler(async (req, res) => {
 module.exports = {
   loginUser,
   getUserProfile,
+  registerUser,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { loginUser, getUserProfile } = require('../controllers/userController');
+const {
+  loginUser,
+  getUserProfile,
+  registerUser,
+} = require('../controllers/userController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
+router.route('/').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/profile').get(protect, getUserProfile);
 
