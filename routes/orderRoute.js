b5642171793ab0d759e389139c71ad9e6c545c98
Hitesh <h08rajbhar@gmail.com>
2023-01-24 19:55:28 +0530

    update order to paidcontroller

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require('../controllers/orderController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 module.exports = router;
