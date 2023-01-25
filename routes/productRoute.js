cd6b23e3b97c4c270fa7f52aecf18a691fc90536
Hitesh <h08rajbhar@gmail.com>
2023-01-25 12:42:01 +0530

    create and updateproduct controller

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,11 +3,17 @@ const {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } = require('../controllers/productController');
 const { protect, admin } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 module.exports = router;
