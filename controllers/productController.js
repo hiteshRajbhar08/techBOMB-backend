07daec3604bbbb8c777fe9656907356ca729a7d2
Hitesh <h08rajbhar@gmail.com>
2023-01-25 12:21:30 +0530

    delete product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -18,7 +18,22 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
+//        ------------ ADMIN ----------------
+//     Delete a product  => /api/products/:id
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
 module.exports = {
   getProducts,
   getProductById,
+  deleteProduct,
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const {
   getProducts,
   getProductById,
+  deleteProduct,
 } = require('../controllers/productController');
+const { protect, admin } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 module.exports = router;
